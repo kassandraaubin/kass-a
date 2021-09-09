8cecdebe03f4939cce27b5f1a5f9fb0d6d3e86b7
Kassandra <kassandra@institutsolacroup.com>
2021-09-09 14:43:46 +0200

    xpush

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import HeroSection from '../components/HeroSection';
 import InfoSection from '../components/InfoSection';
 import Services from '../components/Services';
-import { homeObjOne, homeObjThree, homeObjTwo } from '../components/InfoSection/Data';
+import { homeObjOne, homeObjTwo } from '../components/InfoSection/Data';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 import Footer from '../components/Footer';
